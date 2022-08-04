1cbfb54690681989468273ede621cd3eca4cd4c4
Purushoth9199 <108861486+Purushoth9199@users.noreply.github.com>
2022-08-04 21:20:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,6 @@ request.onload=function(){
     var result=JSON.parse(request.response);
     console.log(result);
 
-    var res=result.map((ele)=>ele.name);
+    var res=result.map((ele)=>ele.flag);
     console.log(res);
-
-    var res1=result.map((ele)=>ele.region);
-    console.log(res1);
-
-    var res2=result.map((ele)=>ele.subregion);
-    console.log(res2);
-
-    var res3=result.map((ele)=>ele.population);
-    console.log(res3);
 }
