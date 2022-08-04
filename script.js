dea139f9662d909b328b7a7e7c6c47c849d53761
Purushoth9199 <108861486+Purushoth9199@users.noreply.github.com>
2022-08-04 21:17:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-var obj1 = {name: "Person 1",age:5};
-var obj2 = {age:5 ,name:"Person 1"};
+var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
 
+    var res=result.map((ele)=>ele.name);
+    console.log(res);
 
-var flag=true;
+    var res1=result.map((ele)=>ele.region);
+    console.log(res1);
 
-if(Object.keys(obj1).length==Object.keys(obj2).length){
-    for(key in obj1) { 
-        if(obj1[key] == obj2[key]) {
-            continue;
-        }
-        else {
-            flag=false;
-            break;
-        }
-    }
+    var res2=result.map((ele)=>ele.subregion);
+    console.log(res2);
+
+    var res3=result.map((ele)=>ele.population);
+    console.log(res3);
 }
-else {
-    flag=false;
-}
-console.log("is object equal"+flag);
